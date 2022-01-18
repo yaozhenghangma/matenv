e48a62e962a77aa2a723e73910970bc95a4a0845
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-01-18 15:37:48 +0800

    update

--- rotation/rotate.py
+++ rotation/rotate.py
@@ -1,6 +1,4 @@
-import ase 
-from ase.build import bulk
-from ase import Atoms
+import ase
 import numpy as np
 
 def BandDirection(positions, lattice):
@@ -27,10 +25,32 @@ def BandDirection(positions, lattice):
     return list
 
 def FindTransformedXYZ(directions):
-    return 0
+    list = []
+    for i in range(0, len(directions)):
+        # right hand helix
+        xyz = np.eye(3)
+        xyz[2,:] = directions[i][0][0]
+        xyz[0,:] = directions[i][1][0]
+        cross_value = np.cross(xyz[2,:], xyz[0,:])
+        possible_y = []
+        for j in range(2, 6):
+            possible_y.append((directions[i][j][0], cross_value.dot(directions[i][j][0])))
+        right_y = max(possible_y, key=lambda x: x[1])
+        xyz[1,:] = right_y[0]
+        # Gram-Schmidt orthogonalization
+        xyz[2,:] = xyz[2,:]/np.linalg.norm(xyz[2,:])
+        xyz[1,:] = xyz[1,:] - np.dot(xyz[2,:], xyz[1,:])*xyz[2,:]
+        xyz[1,:] = xyz[1,:]/np.linalg.norm(xyz[1,:])
+        xyz[0,:] = xyz[0,:] - np.dot(xyz[2,:], xyz[0,:])*xyz[0,:] - np.dot(xyz[1,:], xyz[0,:])*xyz[1,:]
+        xyz[0,:] = xyz[0,:]/np.linalg.norm(xyz[0,:])
+        list.append(xyz)
+    return list
 
 def RotateTo(lattice, transform_matrix):
-    return 0
+    list = []
+    for matrix in transform_matrix:
+        list.append(np.dot(lattice, np.linalg.inv(matrix)))
+    return list
 
 def Rotate(cell):
     positions = cell.get_positions()
@@ -38,8 +58,11 @@ def Rotate(cell):
     rotational_invariant_positions = positions.dot(np.linalg.inv(lattice))
     directions = BandDirection(positions, lattice)
     transform_matrix = FindTransformedXYZ(directions)
-    RotateTo(lattice, transform_matrix)
-    positions = rotational_invariant_positions.dot(lattice)
-    cell.set_positions(positions)
-    cell.cell[:] = lattice
-    return 0
+    lattice_list = RotateTo(lattice, transform_matrix)
+    cell_list = []
+    for new_lattice in lattice_list:
+        positions = rotational_invariant_positions.dot(new_lattice)
+        cell.set_positions(positions)
+        cell.cell[:] = new_lattice
+        cell_list.append(cell)
+    return cell_list
