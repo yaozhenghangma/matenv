0f15a6152a8129c18bfcf118d3fe80be2b460237
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-03-06 14:22:58 +0800

    slice with given Miller index

--- matter_tools/cell/supercell.py
+++ matter_tools/cell/supercell.py
@@ -31,3 +31,63 @@ def enlarge(cell, n=np.ones(3)):
     for i in range(0, len(cell.numbers)):
         cell.numbers[i] = cell.numbers[i] * N
     return cell
+
+
+def slice(cell, miller_index=np.ones(3)):
+    # slice for at least one Miller index being 1
+    non_zero = (miller_index != 0)
+    non_zero_factor = []
+    non_zero_lattice = []
+    zero_lattice = []
+    for i in range(0, 3):
+        if non_zero[i]:
+            non_zero_lattice.append(copy.deepcopy(cell.lattice.lattice[i, 0:3]))
+            non_zero_factor.append(miller_index[i])
+        else :
+            zero_lattice.append(copy.deepcopy(cell.lattice.lattice[i, 0:3]))
+    
+    # a,b: in-plane. c: out-plane
+    if len(non_zero_lattice) == 1:
+        cell.lattice.lattice[2, 0:3] = non_zero_lattice[0]
+        cell.lattice.lattice[0, 0:3] = zero_lattice[0]
+        cell.lattice.lattice[1, 0:3] = zero_lattice[1]
+    elif len(non_zero_lattice) == 2:
+        cell.lattice.lattice[0, 0:3] = zero_lattice[0]
+        if non_zero_factor[0] == 1:
+            cell.lattice.lattice[1, 0:3] = non_zero_factor[1] * non_zero_lattice[0] - non_zero_lattice[1]
+            cell.lattice.lattice[2, 0:3] = non_zero_lattice[0]
+        else :
+            cell.lattice.lattice[1, 0:3] = non_zero_factor[0] * non_zero_lattice[1] - non_zero_lattice[0]
+            cell.lattice.lattice[2, 0:3] = non_zero_lattice[1]
+    elif len(non_zero_lattice) == 3:
+        index = 0
+        for i in range(0, 3):
+            if non_zero_factor[i] == 1:
+                index = i
+                break
+        temp_lattice = copy.deepcopy(non_zero_lattice[0])
+        non_zero_lattice[0] = copy.deepcopy(non_zero_lattice[index])
+        non_zero_lattice[index] = temp_lattice
+        temp_factor = non_zero_factor[0]
+        non_zero_factor[0] = non_zero_factor[index]
+        non_zero_factor[index] = temp_factor
+        cell.lattice.lattice[0, 0:3] = non_zero_factor[1] * non_zero_lattice[0] - non_zero_lattice[1]
+        cell.lattice.lattice[1, 0:3] = non_zero_factor[2] * non_zero_lattice[0] - non_zero_lattice[2]
+        cell.lattice.lattice[2, 0:3] = non_zero_lattice[0]
+
+
+    if np.linalg.det(cell.lattice.lattice) < 0:
+        temp_a = copy.deepcopy(cell.lattice.lattice[0, 0:3])
+        cell.lattice.lattice[0, 0:3] = copy.deepcopy(cell.lattice.lattice[1, 0:3])
+        cell.lattice.lattice[1, 0:3] = temp_a
+    
+    cell.lattice.a = cell.lattice.lattice[0, 0:3]
+    cell.lattice.b = cell.lattice.lattice[1, 0:3]
+    cell.lattice.v = cell.lattice.lattice[2, 0:3]
+
+    reciprocal_lattice = np.linalg.inv(cell.lattice.lattice)
+    for atom in cell.atoms:
+        atom.direct_coordinate = atom.Certesian_coordinate @ reciprocal_lattice
+        atom.direct_coordinate = atom.direct_coordinate - np.floor(atom.direct_coordinate)
+        atom.Certesian_coordinate = atom.direct_coordinate @ cell.lattice.lattice
+    return cell
