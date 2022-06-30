8f66e78c532cb2ab213b2c60571519f7c69448eb
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-06-30 15:22:24 +0800

    add class: SuperCell

--- src/include/cell.cpp
+++ src/include/cell.cpp
@@ -45,6 +45,80 @@ class PYBIND11_EXPORT Cell {
     Atoms atoms;
 };
 
+class PYBIND11_EXPORT Index {
+    public:
+    int ix = 0;
+    int iy = 0;
+    int iz = 0;
+    int atom_index = 0;
+
+    Index(std::vector<int> index);
+};
+
+Index::Index(std::vector<int> index) {
+    this->ix = index[0];
+    this->iy = index[1];
+    this->iz = index[2];
+    this->atom_index = index[3];
+}
+
+class PYBIND11_EXPORT SuperCell {
+    private:
+    int nx=0, ny=0, nz=0;
+    int atoms_per_cell = 0;
+
+    public:
+    Lattice lattice;
+    std::vector<std::vector<std::vector<std::vector<Atom>>>> atoms;
+
+    SuperCell(int nx, int ny, int nz, Cell cell);
+
+    std::vector<int> shape();
+    std::vector<double> coordinate(Index index);
+    std::vector<double> displacement(Index index1, Index index2);
+    double distance(Index index1, Index index2);
+};
+
+SuperCell::SuperCell(int nx, int ny, int nz, Cell cell) {
+    this->nx = nx;
+    this->ny = ny;
+    this->nz = nz;
+    this->atoms_per_cell = cell.atoms.number;
+    this->lattice = cell.lattice;
+    for(int i=0; i<nx; i++) {
+        std::vector<std::vector<std::vector<Atom>>> list_atom2;
+        for(int j=0; j<ny; j++) {
+            std::vector<std::vector<Atom>> list_atom1;
+            for(int k=0; k<nz; k++) {
+                list_atom1.push_back(cell.atoms.atoms);
+            }
+            list_atom2.push_back(list_atom1);
+        }
+        this->atoms.push_back(list_atom2);
+    }
+}
+
+std::vector<int> SuperCell::shape() {
+    return {this->nx, this->ny, this->nz, this->atoms_per_cell};
+}
+
+std::vector<double> SuperCell::coordinate(Index index) {
+    return this->atoms[index.ix][index.iy][index.iz][index.atom_index].coordinate;
+}
+
+std::vector<double> SuperCell::displacement(Index index1, Index index2) {
+    std::vector<double> coordinate1 = this->coordinate(index1);
+    std::vector<double> coordinate2 = this->coordinate(index2);
+    return {coordinate2[0] - coordinate1[0],
+            coordinate2[1] - coordinate1[1],
+            coordinate2[2] - coordinate1[2]};
+}
+
+double SuperCell::distance(Index index1, Index index2) {
+    std::vector<double> displace = this->displacement(index1, index2);
+    return sqrt(displace[0]*displace[0] + displace[1]*displace[1] + displace[2]*displace[2]);
+}
+
 PYBIND11_MODULE(c_cell, m) {
     py::class_<Atom>(m, "Atom")
         .def_readwrite("symbol", &Atom::symbol)
